5a6bc2ab737a975b35baa7b32042ce70fe3c96f5
Tobias Bohwalli <hi@futhr.io>
2013-07-19 17:22:52 +0200

    Append routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.routes.append do
   match '/currency/set', to: 'currency#set', defaults: { format: :json }, as: :set_currency
 
   namespace :admin do
