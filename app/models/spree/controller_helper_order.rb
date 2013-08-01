39cf2ce17b236a37283131e114899cc8295d15fa
Tobias Bohwalli <hi@futhr.io>
2013-08-01 18:21:05 +0200

    Fix for if no currency set.

--- app/models/spree/controller_helper_order.rb
+++ app/models/spree/controller_helper_order.rb
@@ -1,5 +1,5 @@
 Spree::Core::ControllerHelpers::Order.class_eval do
   def current_currency
-    Spree::Currency.current.char_code
+    Spree::Currency.current.try(:char_code) || Spree::Config[:currency]
   end
 end
